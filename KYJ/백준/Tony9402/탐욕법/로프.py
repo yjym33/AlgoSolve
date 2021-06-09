df7cf186c2d278806990525e5c46a98fc8ec4dfd
= <=>
2021-06-10 00:34:13 +0900

    로프 수정

--- KYJ/백준/Tony9402/탐욕법/로프.py
+++ KYJ/백준/Tony9402/탐욕법/로프.py
@@ -1,5 +1,22 @@
 # https://www.acmicpc.net/problem/2217
 
+# 풀이 1
+
+# N = int(input())
+
+# rope = []
+# value = []
+
+# for i in range(N):
+#     rope.append(int(input()))
+# rope.sort(reverse=True)  # 1
+# for num in range(N):
+#     value.append(rope[num] * (num+1))  # 2
+
+# print(max(value))
+
+# 풀이 2
+
 def solution():
     arr.sort(reverse=True) # 내림차순으로 정렬
     for i in range(N):
