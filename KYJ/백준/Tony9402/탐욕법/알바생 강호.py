aca93e0d8b19fa92a5f545f284bf4d9a3bdedb61
= <=>
2021-06-10 23:44:04 +0900

    알바생 강호 수정

--- KYJ/백준/Tony9402/탐욕법/알바생 강호.py
+++ KYJ/백준/Tony9402/탐욕법/알바생 강호.py
@@ -2,17 +2,18 @@
 
 # 풀이 1
 
-N = int(input())
-tip = []
-for _ in range(N):
-    tip.append(a)
-tip.sort(reverse=True)
+n = int(input()) #사람의 수 N이 주어짐
+tip = [] #tip을 넣을 리스트 생성
+for _ in range(n):
+    a = int(input())
+    tip.append(a) 
+tip.sort(reverse = True) #줄 (선사람의 생각하는 팁을 받아와 큰 순서대로 배열
 
-rate = 1
-result = 0
+rate = 1 #등수 초기화
+result = 0 #결과값 초기화
 for i in tip:
-    if (i - (rate-1)) >= 0:
-        result = result + (i- (rate -1))
+    if (i - (rate-1)) >= 0: #(주려고생각한 팁 - (받은등수 - 1)) 이 음수가 아닌 경우만 알고리즘 생성
+        result = result + (i - (rate-1))
         rate += 1
     else:
         continue
@@ -24,24 +25,24 @@ print(result)
 # 하지만 큰 숫자들은 뒤로 갈수록 원래 주려고 했던 팁이 줄어든다. 즉 팁을 최대로 받을 수 있는 최적의 방법에서 멀어진다는 애기이다.
 
 
-# 풀이 2
+# # 풀이 2
 
-import sys
-input = sys.stdin.readline()
-n = int(input())
-arr = [int(input() for _ in range(n))]
+# import sys
+# input = sys.stdin.readline()
+# n = int(input())
+# arr = [int(input() for _ in range(n))]
 
-arr.sort(reverse = True)
-sum = 0
+# arr.sort(reverse = True)
+# sum = 0
 
-for i in range(n):
-    if arr[i] - i < 0:
-        continue
-    sum += (arr[i] - i)
+# for i in range(n):
+#     if arr[i] - i < 0:
+#         continue
+#     sum += (arr[i] - i)
 
-print(sum)
+# print(sum)
 
-#  강호가 최대의 팁을 얻어야 하므로 입력받는 고객의 팁을 역순으로 정렬해서 풀면 된다. 
-# 또한 이후 강호가 받게 되는 팁이 음수가 된다면 아무것도 받지 않는 부분만 코드에 추가해 주면 된다.
+# #  강호가 최대의 팁을 얻어야 하므로 입력받는 고객의 팁을 역순으로 정렬해서 풀면 된다. 
+# # 또한 이후 강호가 받게 되는 팁이 음수가 된다면 아무것도 받지 않는 부분만 코드에 추가해 주면 된다.
 
  
