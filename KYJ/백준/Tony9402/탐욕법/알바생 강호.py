8a80ea53d36d58565093aafb8d001c100c6a1b31
= <=>
2021-06-10 23:33:39 +0900

    알바생 강호 추가

---
+++ KYJ/백준/Tony9402/탐욕법/알바생 강호.py
@@ -0,0 +1,47 @@
+# https://www.acmicpc.net/problem/1758
+
+# 풀이 1
+
+N = int(input())
+tip = []
+for _ in range(N):
+    tip.append(a)
+tip.sort(reverse=True)
+
+rate = 1
+result = 0
+for i in tip:
+    if (i - (rate-1)) >= 0:
+        result = result + (i- (rate -1))
+        rate += 1
+    else:
+        continue
+print(result)
+
+# 이 문제의 핵심은 팁을 받은 순서를 큰 순서대로 리스트에 배열하는 것이다.
+# 만약 1 1 9 9 가 주어진다고 한다면, 작은숫자 즉 1이 줄의 뒤로 가게 된다면
+# 돈 - (받은 등수 - 1)의 값이 음수가 나오게 된다. 따라서 1은 2번쨰 부터 0의 값이 나오게 되므로 어디서 줄을서도 어차피 팁은 0원 이다.
+# 하지만 큰 숫자들은 뒤로 갈수록 원래 주려고 했던 팁이 줄어든다. 즉 팁을 최대로 받을 수 있는 최적의 방법에서 멀어진다는 애기이다.
+
+
+# 풀이 2
+
+import sys
+input = sys.stdin.readline()
+n = int(input())
+arr = [int(input() for _ in range(n))]
+
+arr.sort(reverse = True)
+sum = 0
+
+for i in range(n):
+    if arr[i] - i < 0:
+        continue
+    sum += (arr[i] - i)
+
+print(sum)
+
+#  강호가 최대의 팁을 얻어야 하므로 입력받는 고객의 팁을 역순으로 정렬해서 풀면 된다. 
+# 또한 이후 강호가 받게 되는 팁이 음수가 된다면 아무것도 받지 않는 부분만 코드에 추가해 주면 된다.
+
+ 
--- KYJ/백준/Tony9402/탐욕법/주유소.py
+++ KYJ/백준/Tony9402/탐욕법/주유소.py
@@ -1,2 +1,17 @@
 # https://www.acmicpc.net/problem/13305
 
+n = int(input())
+roads = list(map(int, input()))
+costs = list(map(int, input()))
+
+res = 0
+m = costs[0]
+for i in range(n-1):
+    if costs[i] < m:
+        m = costs[i]
+    res += m * roads[i]
+
+print(res)
+
+# 1. 도시의 기름값 배열을 탐색하며 현재 최저값 보다 더 작은 값이 나오면 갱신하면서 풀어가면 된다.
+# 2. 첫 도시의 기름값을 m으로 두고, roads를 탐색하며 같은 위치의 도시의 기름값이 더 작으면 그 값을 m으로 바꾸고, 도로 길이를 곱한 값을 sum에 더한다.
