f07d0ee0d1cdb483c001f04770caf829410d9c7c
yjym33 <yjym33@naver.com>
2021-07-06 23:16:22 +0900

    미로탐색 수정

--- KYJ/백준/Tony9402/DFS,BFS/미로탐색.py
+++ KYJ/백준/Tony9402/DFS,BFS/미로탐색.py
@@ -1,28 +1,44 @@
 from collections import deque
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+N, M = map(int, input().split())
 
-n, m = map(int, input().split())
-a = [list(map(int, list(input())) for _ in range(n)]
+graph = []
 
-q = deque()
-check = [[False] * m for _ in range(n)]
-dist = [[0] * m for _ in range(n)]
+for _ in range(N):
+  graph.append(list(map(int, input())))
 
-# 시작점
-q.append((0, 0))
-check[0][0] = True
-dist[0][0]  = 1
+# 너비 우선 탐색
+def bfs(x, y):
+  # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+  dx = [-1, 1, 0, 0] 
+  dy = [0, 0, -1, 1]
 
-while q:
-    x, y = q.popleft()
-    for k in range(4):
-        nx, ny = x+dx[k], y+dy[k]
-        if 0 <= nx < n and 0 <= ny < m:
-            if check[nx][ny] == False and a[nx][ny] == 1:
-                q.append((nx, ny))
-                dist[nx][ny] = dist[x][y] + 1
-                check[nx][ny] = True
+  # deque 생성
+  queue = deque()
+  queue.append((x, y))
 
-print(dist[n-1][m-1])
+  while queue:
+    x, y = queue.popleft()
+    
+    # 현재 위치에서 4가지 방향으로 위치 확인
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      # 위치가 벗어나면 안되기 때문에 조건 추가
+      if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        continue
+      
+      # 벽이므로 진행 불가
+      if graph[nx][ny] == 0:
+        continue
+      
+      # 벽이 아니므로 이동
+      if graph[nx][ny] == 1:
+        graph[nx][ny] = graph[x][y] + 1
+        queue.append((nx, ny))
+  
+  # 마지막 값에서 카운트 값을 뽑는다.
+  return graph[N-1][M-1]
+
+print(bfs(0, 0))
